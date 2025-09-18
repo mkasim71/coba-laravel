43f0e08e673042a15a19471c64f45ffcdb321f0b
Muhammad Kasim <mkasim77@gmail.com>
2025-09-19 06:55:36 +0700

    N + 1 Problem

--- routes/web.php
+++ routes/web.php
@@ -51,16 +51,15 @@ return view('categories', [
 });
 
 Route::get('/categories/{category:slug}', function(Category $category) {
-    return view('category', [
-        'title' => $category->name,
-        'posts' => $category->posts,
-        'category' => $category->name
+    return view('posts', [
+        'title' => "Post By Category : $category->name",
+        'posts' => $category->posts->load('author', 'category'),
     ]);
 });
 
 Route::get('/authors/{author:username}', function(User $author) {
     return view('posts', [
-        'title' =>'User Posts',
-        'posts' => $author->posts,
+        'title' =>"Post By Author : $author->name",
+        'posts' => $author->posts->load('category','author'),
     ]);
 });
